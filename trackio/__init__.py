25bef0a41dad59abd634065823dd37ea0afc2cce
Abubakar Abid <abubakar@huggingface.co>
2025-05-12 13:55:02 -0700

    changes

--- trackio/__init__.py
+++ trackio/__init__.py
@@ -12,8 +12,8 @@ current_run: contextvars.ContextVar[Run | None] = contextvars.ContextVar(
     "current_run", default=None
 )
 
-server_running: contextvars.ContextVar[bool] = contextvars.ContextVar(
-    "server_running", default=False
+current_server: contextvars.ContextVar[str | None] = contextvars.ContextVar(
+    "current_server", default=None
 )
 
 config = {}
@@ -21,10 +21,12 @@ config = {}
 
 def init(project, name=None, config=None):
     logging.info(f"Initializing run | Project: {project} | Name: {name}")
-    if not server_running.get():
+    if not current_server.get():
         url = launch_gradio()
-        print(f"Trackio server running at {url}")
-        server_running.set(True)
+        print(f"* Trackio server launched at: {url}")
+    else:
+        url = current_server.get()
+        print(f"* Trackio server already running at: {url}")
     run = Run(project, name, config)
     current_run.set(run)
     globals()["config"] = run.config
